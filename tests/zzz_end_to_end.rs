d5921fef4143f25132d127d5d2e87748ec62267f
Ceri Storey <ceri@lshift.net>
2016-08-24 10:29:22 +0100

    Use randomly generated queue names.

--- tests/zzz_end_to_end.rs
+++ tests/zzz_end_to_end.rs
@@ -1,19 +1,22 @@
 extern crate stomping;
 extern crate env_logger;
 extern crate log;
+extern crate uuid;
 
 use stomping::*;
+use uuid::Uuid;
 
 #[test]
 fn can_round_trip_text() {
     env_logger::init().unwrap_or(());
     let mut client = Client::connect(("localhost", 61613), Some(("guest", "guest"))).expect("connect");
     let body = b"42";
+    let queue = format!("/queue/can_round_trip_text-{}", Uuid::new_v4());
 
-    client.subscribe("/queue/can_blah", "one", AckMode::Auto).expect("subscribe");
-    client.publish("/queue/can_blah", body).expect("publish");
+    client.subscribe(&queue, "one", AckMode::Auto).expect("subscribe");
+    client.publish(&queue, body).expect("publish");
 
-    let (headers, msg) = client.consume_next().expect("consume_next");
+    let (_headers, msg) = client.consume_next().expect("consume_next");
     assert_eq!(msg, body);
 }
 
@@ -22,10 +25,11 @@ fn can_round_trip_binary_blobs() {
     env_logger::init().unwrap_or(());
     let mut client = Client::connect(("localhost", 61613), Some(("guest", "guest"))).expect("connect");
     let body = b"\x00\x01\x02\x03";
+    let queue = format!("/queue/can_round_trip_binary_blobs-{}", Uuid::new_v4());
 
-    client.subscribe("/queue/can_blah", "one", AckMode::Auto).expect("subscribe");
-    client.publish("/queue/can_blah", body).expect("publish");
+    client.subscribe(&queue, "one", AckMode::Auto).expect("subscribe");
+    client.publish(&queue, body).expect("publish");
 
-    let (headers, msg) = client.consume_next().expect("consume_next");
+    let (_headers, msg) = client.consume_next().expect("consume_next");
     assert_eq!(msg, body);
 }

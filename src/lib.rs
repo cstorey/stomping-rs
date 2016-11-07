6e9f57e846494538a8f6f4bb1c708c3176d014b0
Ceri Storey <cez@necrofish.org.uk>
2016-11-07 21:40:03 +0000

    Make the timeout calculation a bit more precise.

--- src/lib.rs
+++ src/lib.rs
@@ -74,9 +74,9 @@ fn parse_keepalive(headervalue: Option<&str>) -> Result<(Option<Duration>, Optio
         info!("heartbeat: theirs:{:?}", sxsy);
         let mut it = sxsy.trim().splitn(2, ',');
         let sx = Duration::from_millis(try!(try!(it.next().ok_or(ErrorKind::ProtocolError))
-                                                .parse()));
+            .parse()));
         let sy = Duration::from_millis(try!(try!(it.next().ok_or(ErrorKind::ProtocolError))
-                                                .parse()));
+            .parse()));
         info!("heartbeat: theirs:{:?}", (&sx, &sy));
 
         Ok((some_non_zero(sx), some_non_zero(sy)))
@@ -99,6 +99,7 @@ impl Client {
                                      credentials: Option<(&str, &str)>,
                                      keepalive: Option<Duration>)
                                      -> Result<Self> {
+        let start = SystemTime::now();
         let wr = try!(TcpStream::connect(a));
         debug!("connected to: {:?}", try!(wr.peer_addr()));
         try!(wr.set_read_timeout(keepalive.map(|d| d * 3)));
@@ -107,7 +108,7 @@ impl Client {
         let mut client = Client {
             wr: BufWriter::new(wr),
             rdr: BufReader::new(rdr),
-            pace: PaceMaker::default(),
+            pace: PaceMaker::new(None, None, None, start),
         };
         conn_headers.insert("accept-version".to_string(), "1.2".to_string());
         if let &Some(ref duration) = &keepalive {
@@ -131,12 +132,17 @@ impl Client {
         }
 
         let (sx, sy) = try!(parse_keepalive(hdrs.get("heart-beat").map(|s| &**s)));
-        client.pace = PaceMaker::new(keepalive, sx, sy);
+        client.pace = PaceMaker::new(keepalive, sx, sy, start);
 
-        try!(client.wr.get_mut().set_read_timeout(client.pace.read_timeout()));
-        try!(client.rdr.get_mut().set_read_timeout(client.pace.read_timeout()));
-
+        try!(client.reset_timeouts());
         Ok(client)
+    }
+
+    fn reset_timeouts(&mut self) -> Result<()> {
+        let now = SystemTime::now();
+        try!(self.wr.get_mut().set_read_timeout(self.pace.read_timeout(now)));
+        try!(self.rdr.get_mut().set_read_timeout(self.pace.read_timeout(now)));
+        Ok(())
     }
 
     pub fn subscribe(&mut self, destination: &str, id: &str, mode: AckMode) -> Result<()> {
@@ -189,6 +195,7 @@ impl Client {
         try!(self.wr.write(b"\0"));
         try!(self.wr.flush());
         self.pace.write_observed(SystemTime::now());
+        try!(self.reset_timeouts());
         Ok(())
     }
 
@@ -199,15 +206,17 @@ impl Client {
             match result {
                 Ok(_) => {
                     self.pace.read_observed(SystemTime::now());
+                    try!(self.reset_timeouts());
                     return Ok(());
                 }
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                     match try!(self.pace.handle_read_timeout(SystemTime::now())) {
-                        BeatAction::Retry => continue,
+                        BeatAction::Retry => try!(self.reset_timeouts()),
                         BeatAction::SendClientHeart => {
                             try!(self.wr.write_all(b"\n"));
                             try!(self.wr.flush());
                             self.pace.write_observed(SystemTime::now());
+                            try!(self.reset_timeouts());
                             debug!("Sent heartbeat");
                         }
                         _ => unimplemented!(),
@@ -268,12 +277,12 @@ impl Client {
 
 
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone)]
 struct PaceMaker {
     client_to_server: Option<Duration>,
     server_to_client: Option<Duration>,
-    last_observed_read: Option<SystemTime>,
-    last_observed_write: Option<SystemTime>,
+    last_observed_read: SystemTime,
+    last_observed_write: SystemTime,
 }
 
 #[derive(Debug, Clone, PartialEq,Eq)]
@@ -284,43 +293,59 @@ enum BeatAction {
 }
 
 impl PaceMaker {
-    fn new(keepalive: Option<Duration>, sx: Option<Duration>, sy: Option<Duration>) -> Self {
-        debug!("heart-beat: cx, cy:{:?}; server-transmit:{:?}; server-receive:{:?}",
+    fn new(keepalive: Option<Duration>,
+           sx: Option<Duration>,
+           sy: Option<Duration>,
+           now: SystemTime)
+           -> Self {
+        debug!("heart-beat: cx, cy:{:?}; server-transmit:{:?}; server-receive:{:?}; now:{:?}",
                keepalive,
                sx,
-               sy);
+               sy,
+               now);
         let client_to_server = sy.and_then(|sy| keepalive.map(|cx| cmp::max(cx, sy)));
         let server_to_client = sx.and_then(|sx| keepalive.map(|cy| cmp::max(cy, sx)));
 
         PaceMaker {
             client_to_server: client_to_server,
             server_to_client: server_to_client,
-            last_observed_read: None,
-            last_observed_write: None,
+            last_observed_read: now,
+            last_observed_write: now,
         }
     }
 
-    fn read_timeout(&self) -> Option<Duration> {
-        match (self.server_to_client, self.client_to_server) {
-            (Some(s2c), Some(c2s)) => Some(cmp::min(c2s / 2, s2c * 2)),
-            (Some(s2c), None) => Some(s2c * 2),
-            (None, Some(c2s)) => Some(c2s / 2),
-            _ => None,
-        }
+    fn read_timeout(&self, now: SystemTime) -> Option<Duration> {
+        trace!("read_timeout:{:?}", self);
+        let default = Duration::from_millis(100);
+        let until_read = self.server_to_client
+            .map(|s2c| {
+                let res = (self.last_observed_read + s2c).duration_since(now);
+                trace!("s2c timeout:{:?}; due in {:?}", s2c, res);
+                res.unwrap_or(default) * 2
+            });
+        let until_write = self.client_to_server
+            .map(|c2s| {
+                let res = (self.last_observed_write + c2s).duration_since(now);
+                trace!("c2s timeout:{:?}; due in {:?}", c2s, res);
+                res.unwrap_or(default) / 2
+            });
+        let min = until_read.into_iter().chain(until_write.into_iter()).min();
+        debug!("overall due in {:?}", min);
+        return min;
     }
 
     fn read_observed(&mut self, at: SystemTime) {
-        self.last_observed_read = Some(at);
+        self.last_observed_read = at;
         debug!("last_observed_read now: {:?}", at);
     }
     fn write_observed(&mut self, at: SystemTime) {
-        self.last_observed_write = Some(at);
+        self.last_observed_write = at;
         debug!("last_observed_write now: {:?}", at);
     }
 
     fn handle_read_timeout(&mut self, at: SystemTime) -> Result<BeatAction> {
         debug!("handle_read_timeout: {:?} at {:?}", self, &at);
-        if let (Some(mark), Some(interval)) = (self.last_observed_write, self.client_to_server) {
+        if let (mark, Some(interval)) = (self.last_observed_write, self.client_to_server) {
             let duration = try!(at.duration_since(mark));
             debug!("consider sending heartbeat after: {:?} - {:?} -> {:?}",
                    mark,
@@ -332,7 +357,7 @@ impl PaceMaker {
             }
         }
 
-        if let (Some(mark), Some(interval)) = (self.last_observed_read, self.server_to_client) {
+        if let (mark, Some(interval)) = (self.last_observed_read, self.server_to_client) {
             let duration = try!(at.duration_since(mark));
             debug!("considering if alive after: {:?} - {:?} -> {:?}",
                    mark,
@@ -386,50 +411,59 @@ mod test {
     #[test]
     fn pacemaker_blah_blah_blah() {
         env_logger::init().unwrap_or(());
-        let pm = PaceMaker::new(None, None, None);
-        assert_eq!(pm.read_timeout(), None);
+        let start = SystemTime::now();
+        let pm = PaceMaker::new(None, None, None, start);
+        assert_eq!(pm.read_timeout(start), None);
     }
 
     #[test]
     fn pacemaker_inf_read_timeout_when_server_unsupported() {
         env_logger::init().unwrap_or(());
-        let pm = PaceMaker::new(Some(Duration::from_millis(20)), None, None);
+        let start = SystemTime::now();
+        let pm = PaceMaker::new(Some(Duration::from_millis(20)), None, None, start);
         println!("pm: {:?}", pm);
-        println!("read_timeout: {:?}", pm.read_timeout());
-        assert_eq!(pm.read_timeout(), None);
+        println!("read_timeout: {:?}", pm.read_timeout(start));
+        let start = SystemTime::now();
+        assert_eq!(pm.read_timeout(start), None);
     }
 
     #[test]
     fn pacemaker_read_timeout_max_of_ours_and_server_send_rate_times_two() {
         env_logger::init().unwrap_or(());
+        let start = SystemTime::now();
         let pm = PaceMaker::new(Some(Duration::from_millis(20)),
                                 Some(Duration::from_millis(10)),
-                                None);
+                                None,
+                                start);
         println!("pm: {:?}", pm);
-        println!("read_timeout: {:?}", pm.read_timeout());
-        assert_eq!(pm.read_timeout(), Some(Duration::from_millis(40)));
+        println!("read_timeout: {:?}", pm.read_timeout(start));
+        assert_eq!(pm.read_timeout(start), Some(Duration::from_millis(40)));
     }
 
     #[test]
     fn pacemaker_read_timeout_max_of_ours_and_server_send_rate_times_two_2() {
         env_logger::init().unwrap_or(());
+        let start = SystemTime::now();
         let pm = PaceMaker::new(Some(Duration::from_millis(20)),
                                 Some(Duration::from_millis(30)),
-                                None);
+                                None,
+                                start);
         println!("pm: {:?}", pm);
-        println!("read_timeout: {:?}", pm.read_timeout());
-        assert_eq!(pm.read_timeout(), Some(Duration::from_millis(60)));
+        println!("read_timeout: {:?}", pm.read_timeout(start));
+        assert_eq!(pm.read_timeout(start), Some(Duration::from_millis(60)));
     }
 
     #[test]
     fn pacemaker_read_timeout_should_be_half_client_heartbeat() {
         env_logger::init().unwrap_or(());
+        let start = SystemTime::now();
         let pm = PaceMaker::new(Some(Duration::from_millis(10)),
                                 None,
-                                Some(Duration::from_millis(30)));
+                                Some(Duration::from_millis(30)),
+                                start);
         println!("pm: {:?}", pm);
-        println!("read_timeout: {:?}", pm.read_timeout());
-        assert_eq!(pm.read_timeout(), Some(Duration::from_millis(15)));
+        println!("read_timeout: {:?}", pm.read_timeout(start));
+        assert_eq!(pm.read_timeout(start), Some(Duration::from_millis(15)));
     }
 
     #[test]
@@ -439,27 +473,31 @@ mod test {
         // Server wants to send every 10ms, receive every 30ms.
         // -> We need to send one every 30ms, we expect one every 10ms.
         // So if we don't see any reads after 20ms, we consider the peer dead.
+        let start = SystemTime::now();
         let pm = PaceMaker::new(Some(Duration::from_millis(10)),
                                 Some(Duration::from_millis(10)),
-                                Some(Duration::from_millis(30)));
+                                Some(Duration::from_millis(30)),
+                                start);
         println!("pm: {:?}", pm);
-        println!("read_timeout: {:?}", pm.read_timeout());
+        println!("read_timeout: {:?}", pm.read_timeout(start));
         assert_eq!(pm.client_to_server, Some(Duration::from_millis(30)));
         assert_eq!(pm.server_to_client, Some(Duration::from_millis(10)));
-        assert_eq!(pm.read_timeout(), Some(Duration::from_millis(15)));
+        assert_eq!(pm.read_timeout(start), Some(Duration::from_millis(15)));
     }
 
     #[test]
     fn pacemaker_read_timeout_should_be_min_of_client_and_twice_server_heartbeat_2() {
         env_logger::init().unwrap_or(());
+        let start = SystemTime::now();
         let pm = PaceMaker::new(Some(Duration::from_millis(10)),
                                 Some(Duration::from_millis(10)),
-                                Some(Duration::from_millis(2)));
+                                Some(Duration::from_millis(2)),
+                                start);
         println!("pm: {:?}", pm);
-        println!("read_timeout: {:?}", pm.read_timeout());
+        println!("read_timeout: {:?}", pm.read_timeout(start));
         assert_eq!(pm.client_to_server, Some(Duration::from_millis(10)));
         assert_eq!(pm.server_to_client, Some(Duration::from_millis(10)));
-        assert_eq!(pm.read_timeout(), Some(Duration::from_millis(5)));
+        assert_eq!(pm.read_timeout(start), Some(Duration::from_millis(5)));
     }
 
     #[test]
@@ -469,14 +507,16 @@ mod test {
         // -> We need to send one every 2ms, we expect one every 10ms.
         // So if we don't see any reads after 2ms, wakeup and send frame.
         env_logger::init().unwrap_or(());
+        let start = SystemTime::now();
         let pm = PaceMaker::new(Some(Duration::from_millis(2)),
                                 Some(Duration::from_millis(10)),
-                                Some(Duration::from_millis(2)));
+                                Some(Duration::from_millis(2)),
+                                start);
         println!("pm: {:?}", pm);
-        println!("read_timeout: {:?}", pm.read_timeout());
+        println!("read_timeout: {:?}", pm.read_timeout(start));
         assert_eq!(pm.client_to_server, Some(Duration::from_millis(2)));
         assert_eq!(pm.server_to_client, Some(Duration::from_millis(10)));
-        assert_eq!(pm.read_timeout(), Some(Duration::from_millis(1)));
+        assert_eq!(pm.read_timeout(start), Some(Duration::from_millis(1)));
     }
 
     #[test]
@@ -485,13 +525,14 @@ mod test {
         let start = SystemTime::now();
         let mut pm = PaceMaker::new(Some(Duration::from_millis(10)),
                                     Some(Duration::from_millis(10)),
-                                    None);
+                                    None,
+                                    start);
         pm.read_observed(start);
         assert_eq!(pm.handle_read_timeout(start + Duration::from_millis(19))
-                     .expect("handle_read_timeout"),
+                       .expect("handle_read_timeout"),
                    BeatAction::Retry);
         assert_eq!(pm.handle_read_timeout(start + Duration::from_millis(20))
-                     .expect("handle_read_timeout"),
+                       .expect("handle_read_timeout"),
                    BeatAction::PeerFailed);
     }
 
@@ -501,13 +542,14 @@ mod test {
         let start = SystemTime::now();
         let mut pm = PaceMaker::new(Some(Duration::from_millis(10)),
                                     None,
-                                    Some(Duration::from_millis(10)));
+                                    Some(Duration::from_millis(10)),
+                                    start);
         pm.write_observed(start);
         assert_eq!(pm.handle_read_timeout(start + Duration::from_millis(9))
-                     .expect("handle_read_timeout"),
+                       .expect("handle_read_timeout"),
                    BeatAction::Retry);
         assert_eq!(pm.handle_read_timeout(start + Duration::from_millis(10))
-                     .expect("handle_read_timeout"),
+                       .expect("handle_read_timeout"),
                    BeatAction::SendClientHeart);
     }
 

9541d7765099463b18227bb5343bb226f011d358
Ceri Storey <cez@necrofish.org.uk>
2019-12-19 14:31:02 +0000

    Handle colons.

--- src/unparser.rs
+++ src/unparser.rs
@@ -1,6 +1,7 @@
 #![cfg(test)]
 
 use bytes::{BufMut, BytesMut};
+use maplit::*;
 
 use crate::errors::*;
 use crate::{Command, Frame, Headers};
@@ -8,6 +9,23 @@ use crate::{Command, Frame, Headers};
 fn encode_frame(buf: &mut BytesMut, frame: &Frame) -> Result<()> {
     buf.put_slice(frame.command.as_str().as_bytes());
     buf.put_u8(b'\n');
+
+    for (k, v) in frame.headers.iter() {
+        for c in k.as_bytes() {
+            match *c {
+                b':' => buf.put_slice(b"\\c"),
+                _ => buf.put_u8(*c),
+            }
+        }
+        buf.put_u8(b':');
+        for c in v.as_bytes() {
+            match *c {
+                b':' => buf.put_slice(b"\\c"),
+                _ => buf.put_u8(*c),
+            }
+        }
+        buf.put_u8(b'\n');
+    }
 
     buf.put_u8(b'\n');
 
@@ -31,6 +49,60 @@ mod tests {
 
         encode_frame(&mut buf, &frame).expect("encode frame");
 
-        assert_eq!(&*b"SEND\n\n\0", &*buf);
+        assert_eq!(
+            &*"SEND\n\n\0",
+            std::str::from_utf8(&buf).expect("from utf8")
+        );
+    }
+
+    #[test]
+    fn should_encode_a_header() {
+        let frame = Frame {
+            command: Command::Send,
+            headers: btreemap! {"hello".into() => "world".into()},
+            body: Vec::new(),
+        };
+        let mut buf = BytesMut::new();
+
+        encode_frame(&mut buf, &frame).expect("encode frame");
+
+        assert_eq!(
+            &*"SEND\nhello:world\n\n\0",
+            std::str::from_utf8(&buf).expect("from utf8")
+        );
+    }
+
+    #[test]
+    fn should_encode_a_header_colon_in_name() {
+        let frame = Frame {
+            command: Command::Send,
+            headers: btreemap! {"foo:bar".into() => "y".into()},
+            body: Vec::new(),
+        };
+        let mut buf = BytesMut::new();
+
+        encode_frame(&mut buf, &frame).expect("encode frame");
+
+        assert_eq!(
+            &*"SEND\nfoo\\cbar:y\n\n\0",
+            std::str::from_utf8(&buf).expect("from utf8")
+        );
+    }
+
+    #[test]
+    fn should_encode_a_header_colon_in_value() {
+        let frame = Frame {
+            command: Command::Send,
+            headers: btreemap! {"y".into() => "foo:bar".into()},
+            body: Vec::new(),
+        };
+        let mut buf = BytesMut::new();
+
+        encode_frame(&mut buf, &frame).expect("encode frame");
+
+        assert_eq!(
+            &*"SEND\ny:foo\\cbar\n\n\0",
+            std::str::from_utf8(&buf).expect("from utf8")
+        );
     }
 }

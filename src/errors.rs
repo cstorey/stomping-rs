4475ac2ea72f1652ef6d39c2ca4602abd26be10d
Ceri Storey <cez@necrofish.org.uk>
2020-03-28 10:59:30 +0000

    Get rid of old unused error chain use.

--- src/errors.rs
+++ src/errors.rs
@@ -34,28 +34,3 @@ pub enum StompError {
     #[error("Timeout: {0}")]
     TimedOut(#[from] tokio::time::Elapsed),
 }
-
-#[cfg(never)]
-error_chain! (
-    foreign_links {
-        io::Error, Io;
-        num::ParseIntError, ParseInt;
-        time::SystemTimeError, SystemTime;
-    }
-
-    errors {
-        StompError(command: String, headers:BTreeMap<String, String>, body: String) {
-            description("stomp error")
-            display("stomp error: {}: {:?}: {:?}", command, headers, body)
-        }
-        ProtocolError {
-            description("protocol error")
-        }
-        NoAckHeader {
-            description("Tried to ack a frame with no `ack` header")
-        }
-        PeerFailed {
-            description("peer seems to be unresponsive")
-        }
-    }
-);

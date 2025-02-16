1acd9a72909f6ee30b014e16a235abd181349c12
C Storey <git@noncrab.net>
2025-02-16 14:42:33 +0000

    Use results consistently.

--- tests/zzz_end_to_end.rs
+++ tests/zzz_end_to_end.rs
@@ -2,7 +2,7 @@
 
 use std::time::Duration;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use futures::stream::StreamExt;
 use stomping::{Client, StompError, *};
 use tokio::{task::JoinHandle, time::timeout};
@@ -10,9 +10,9 @@ use tracing::{debug, info};
 use uuid::Uuid;
 
 #[tokio::test]
-async fn can_round_trip_text() {
+async fn can_round_trip_text() -> Result<()> {
     tracing_subscriber::fmt::try_init().unwrap_or_default();
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let body = b"42";
     let queue = format!("/queue/can_round_trip_text-{}", Uuid::new_v4());
@@ -21,25 +21,25 @@ async fn can_round_trip_text() {
     let mut sub: Subscription = client
         .subscribe(&queue, "one", AckMode::Auto, Default::default())
         .await
-        .expect("subscribe");
+        .context("subscribe")?;
 
     info!("Publishing to queue");
-    client.publish(&queue, body).await.expect("publish");
+    client.publish(&queue, body).await.context("publish")?;
 
     info!("Consuming from queue");
-    let frame: Frame = sub.next().await.expect("consume_next");
+    let frame: Frame = sub.next().await.context("consume_next")?;
     info!("Consumed item");
 
     assert_eq!(body, &*frame.body);
-    client.disconnect().await.expect("disconnect");
-    let res = conn_task.await;
-    assert!(res.is_ok(), "Conection exited normally");
+    client.disconnect().await.context("disconnect")?;
+    conn_task.await??;
+    Ok(())
 }
 
 #[tokio::test]
-async fn can_round_trip_binary_blobs() {
+async fn can_round_trip_binary_blobs() -> Result<()> {
     tracing_subscriber::fmt::try_init().unwrap_or_default();
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let body = b"\x00\x01\x02\x03";
     let queue = format!("/queue/can_round_trip_binary_blobs-{}", Uuid::new_v4());
@@ -47,20 +47,20 @@ async fn can_round_trip_binary_blobs() {
     let mut sub = client
         .subscribe(&queue, "one", AckMode::Auto, Default::default())
         .await
-        .expect("subscribe");
-    client.publish(&queue, body).await.expect("publish");
+        .context("subscribe")?;
+    client.publish(&queue, body).await.context("publish")?;
 
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(frame.body, body);
-    client.disconnect().await.expect("disconnect");
-    let res = conn_task.await;
-    assert!(res.is_ok(), "Conection exited normally");
+    client.disconnect().await.context("disconnect")?;
+    conn_task.await??;
+    Ok(())
 }
 
 #[tokio::test]
-async fn client_acks_should_allow_redelivery() {
+async fn client_acks_should_allow_redelivery() -> Result<()> {
     tracing_subscriber::fmt::try_init().unwrap_or_default();
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let body = b"42";
     let queue = format!(
@@ -71,40 +71,40 @@ async fn client_acks_should_allow_redelivery() {
     let mut sub = client
         .subscribe(&queue, "one", AckMode::ClientIndividual, Default::default())
         .await
-        .expect("subscribe");
-    client.publish(&queue, body).await.expect("publish");
+        .context("subscribe")?;
+    client.publish(&queue, body).await.context("publish")?;
 
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(frame.body, body);
 
     debug!("Disconnecting without acking");
     // Disconnect
     drop(sub);
-    client.disconnect().await.expect("disconnect");
+    client.disconnect().await.context("disconnect")?;
     debug!("Dropped client; awaiting connection");
     let res = conn_task.await;
     assert!(res.is_ok(), "Conection exited normally");
     debug!("First connection done");
 
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let mut sub = client
         .subscribe(&queue, "one", AckMode::ClientIndividual, Default::default())
         .await
-        .expect("subscribe");
+        .context("subscribe")?;
     info!("Subscribed on second connection; awaiting next");
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(frame.body, body);
-    client.disconnect().await.expect("disconnect");
-    let res = conn_task.await;
-    assert!(res.is_ok(), "Conection exited normally");
+    client.disconnect().await.context("disconnect")?;
+    conn_task.await??;
+    Ok(())
 }
 
 #[tokio::test]
-async fn can_encode_headers_correctly() {
+async fn can_encode_headers_correctly() -> Result<()> {
     tracing_subscriber::fmt::try_init().unwrap_or_default();
     debug!("Connecting");
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let body = b"42";
     let queue = format!("/queue/can_encode_headers_correctly:{}", Uuid::new_v4());
@@ -112,21 +112,21 @@ async fn can_encode_headers_correctly() {
     let mut sub = client
         .subscribe(&queue, "one", AckMode::Auto, Default::default())
         .await
-        .expect("subscribe");
-    client.publish(&queue, body).await.expect("publish");
+        .context("subscribe")?;
+    client.publish(&queue, body).await.context("publish")?;
 
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     println!("h: {:?}", frame.headers);
     assert_eq!(frame.headers["destination"], queue);
-    client.disconnect().await.expect("disconnect");
-    let res = conn_task.await;
-    assert!(res.is_ok(), "Conection exited normally");
+    client.disconnect().await.context("disconnect")?;
+    conn_task.await??;
+    Ok(())
 }
 
 #[tokio::test]
-async fn should_allow_acking_individual_messages() {
+async fn should_allow_acking_individual_messages() -> Result<()> {
     tracing_subscriber::fmt::try_init().unwrap_or_default();
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let queue = format!(
         "/queue/should_allow_acking_individual_messages-{}",
@@ -136,20 +136,20 @@ async fn should_allow_acking_individual_messages() {
     let mut sub = client
         .subscribe(&queue, "one", AckMode::ClientIndividual, Default::default())
         .await
-        .expect("subscribe");
-    client.publish(&queue, b"first").await.expect("publish");
-    client.publish(&queue, b"second").await.expect("publish");
-    client.publish(&queue, b"third").await.expect("publish");
+        .context("subscribe")?;
+    client.publish(&queue, b"first").await.context("publish")?;
+    client.publish(&queue, b"second").await.context("publish")?;
+    client.publish(&queue, b"third").await.context("publish")?;
 
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(frame.body, b"first");
     debug!("Consumed first");
 
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(frame.body, b"second");
     debug!("Consumed second");
 
-    client.ack(&frame.headers).await.expect("ack");
+    client.ack(&frame.headers).await.context("ack")?;
     debug!("Acked second");
 
     // If we don't consume all of the items on the channel, we risk a deadlock.
@@ -157,13 +157,13 @@ async fn should_allow_acking_individual_messages() {
     // third message to the client, meaning it's unable to observe the receipt
     // frame from the disconnect.
     // We should find
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(frame.body, b"third");
     debug!("Observed third");
 
     info!("Disconnecting…");
     // Disconnect
-    client.disconnect().await.expect("disconnect");
+    client.disconnect().await.context("disconnect")?;
     debug!("Drop subscription");
     drop(sub);
     info!("Disconnected…");
@@ -172,47 +172,47 @@ async fn should_allow_acking_individual_messages() {
     assert!(res.is_ok(), "Conection exited normally");
     info!("Disconnected first connection; reconnecting");
 
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let mut sub = client
         .subscribe(&queue, "one", AckMode::ClientIndividual, Default::default())
         .await
-        .expect("subscribe");
-    let frame = sub.next().await.expect("consume_next");
+        .context("subscribe")?;
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(std::str::from_utf8(&frame.body).unwrap(), "first");
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(std::str::from_utf8(&frame.body).unwrap(), "third");
 
-    client.disconnect().await.expect("disconnect");
-    let res = conn_task.await;
-    assert!(res.is_ok(), "Conection exited normally");
+    client.disconnect().await.context("disconnect")?;
+    conn_task.await??;
+    Ok(())
 }
 
 // This test never actually terminates.
 #[tokio::test]
 #[ignore]
-async fn thing_to_test_timeouts() {
+async fn thing_to_test_timeouts() -> Result<()> {
     tracing_subscriber::fmt::try_init().unwrap_or_default();
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let queue = format!("/queue/thing_to_test_timeouts-{}", Uuid::new_v4());
 
     let mut sub = client
         .subscribe(&queue, "one", AckMode::ClientIndividual, Default::default())
         .await
-        .expect("subscribe");
+        .context("subscribe")?;
 
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(frame.body, b"first");
-    client.disconnect().await.expect("disconnect");
-    let res = conn_task.await;
-    assert!(res.is_ok(), "Conection exited normally");
+    client.disconnect().await.context("disconnect")?;
+    conn_task.await??;
+    Ok(())
 }
 
 #[tokio::test]
-async fn should_allow_disconnect_by_dropping_with_pending_deliveries() {
+async fn should_allow_disconnect_by_dropping_with_pending_deliveries() -> Result<()> {
     tracing_subscriber::fmt::try_init().unwrap_or_default();
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let queue = format!(
         "/queue/should_allow_disconnect_by_dropping_with_pending_deliveries-{}",
@@ -222,13 +222,13 @@ async fn should_allow_disconnect_by_dropping_with_pending_deliveries() {
     let mut sub = client
         .subscribe(&queue, "one", AckMode::ClientIndividual, Default::default())
         .await
-        .expect("subscribe");
-    client.publish(&queue, b"first").await.expect("publish");
-    client.publish(&queue, b"second").await.expect("publish");
-    client.publish(&queue, b"third").await.expect("publish");
+        .context("subscribe")?;
+    client.publish(&queue, b"first").await.context("publish")?;
+    client.publish(&queue, b"second").await.context("publish")?;
+    client.publish(&queue, b"third").await.context("publish")?;
 
     // A good-enough proxy for checking that we've had some deliveries.
-    let frame = sub.next().await.expect("consume_next");
+    let frame = sub.next().await.context("consume_next")?;
     assert_eq!(frame.body, b"first");
     debug!("Consumed first");
 
@@ -237,15 +237,14 @@ async fn should_allow_disconnect_by_dropping_with_pending_deliveries() {
     drop(client);
     info!("Disconnected…");
 
-    let res = conn_task.await;
-    assert!(res.is_ok(), "Conection exited normally");
-    info!("Disconnected first connection; reconnecting");
+    conn_task.await??;
+    Ok(())
 }
 
 #[tokio::test]
-async fn should_fail_when_we_force_an_error() {
+async fn should_fail_when_we_force_an_error() -> Result<()> {
     tracing_subscriber::fmt::try_init().unwrap_or_default();
-    let (conn_task, mut client) = connect_to_stomp().await.expect("connect");
+    let (conn_task, mut client) = connect_to_stomp().await.context("connect")?;
 
     let queue = format!("/invalid-thing/can_round_trip_text-{}", Uuid::new_v4());
 
@@ -255,22 +254,24 @@ async fn should_fail_when_we_force_an_error() {
     let mut _sub: Subscription = client
         .subscribe(&queue, "one", AckMode::Auto, Default::default())
         .await
-        .expect("subscribe succeeds ok");
+        .context("subscribe succeeds ok")?;
 
     let res = timeout(Duration::from_millis(1000), conn_task)
         .await
-        .expect("no timeout")
-        .expect("joins okay");
+        .context("no timeout")?
+        .context("joins okay")?;
 
     let err = res.expect_err("has error");
 
     match err
         .downcast::<StompError>()
-        .expect("downcast to StompError")
+        .context("downcast to StompError")?
     {
         StompError::StompError { .. } => {}
         e => panic!("Unexpected error: Got: {:?}", e),
     }
+
+    Ok(())
 }
 
 async fn connect_to_stomp() -> Result<(JoinHandle<Result<()>>, Client)> {
